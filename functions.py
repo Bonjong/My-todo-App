3885c1a83298ed50882038c8a4c8f2623e2f07b2
jamyangsonam <bonjong85@gmail.com>
2025-03-06 22:55:03 +0600

    Initial commit

---
+++ functions.py
@@ -0,0 +1,12 @@
+def get_todos(filepath="todos.txt"):
+    with open(filepath,'r')as file:
+        todos=file.readlines()
+    return todos
+
+
+def write_todo(todos_arg,filepath="todos.txt"):
+    with open(filepath,"w")as file:
+        file.writelines(todos_arg)
+
+
+
